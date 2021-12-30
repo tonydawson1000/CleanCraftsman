d739c372f4ef377a5d9a0ef4dfdde8c64f3b359e
tonydawson1000 <tonydawson1000@gmail.com>
2021-12-30 11:30:59 +0000

    Added simple two-element FIFI Stack using TDD

---
+++ CleanCraftsman/Stack.cs
@@ -0,0 +1,42 @@
+﻿namespace CleanCraftsman
+{
+    public class Stack
+    {
+        private int size = 0;
+        private int[] elements = new int[2];
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public int Pop()
+        {
+            if (size == 0)
+                throw new UnderflowException("Stack is Empty, Nothing to Pop");
+
+            return elements[--size];
+        }
+
+        public void Push(int element)
+        {
+            this.elements[size++] = element;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+    }
+
+    public class UnderflowException : Exception
+    {
+        public UnderflowException() { }
+
+        public UnderflowException(string message)
+            : base(message) { }
+
+        public UnderflowException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
---
+++ CleanCraftsmanTests/StackTest.cs
@@ -0,0 +1,99 @@
+﻿using CleanCraftsman;
+using System;
+using Xunit;
+
+namespace CleanCraftsmanTests
+{
+    public class StackTest
+    {
+        private readonly Stack stack = new();
+
+        [Fact]
+        public void AfterPushingXAndY_WillPopYThenX()
+        {
+            //Arrange
+
+            //Act
+            stack.Push(99);
+            stack.Push(88);
+
+            //Assert
+            Assert.Equal(88, stack.Pop());
+            Assert.Equal(99, stack.Pop());
+        }
+
+        [Fact]
+        public void AfterPushingX_WillPopX()
+        {
+            //Arrange
+
+            //Act
+            stack.Push(99);
+
+            //Assert
+            Assert.Equal(99, stack.Pop());
+
+
+            //Act
+            stack.Push(88);
+
+            //Assert
+            Assert.Equal(88, stack.Pop());
+        }
+
+        [Fact]
+        public void PoppingEmptyStack_ThrowsUnderflow()
+        {
+            //Arrange
+
+            //Act
+            Action act = () => stack.Pop();
+
+            //Assert
+            UnderflowException exception = Assert.Throws<UnderflowException>(act);
+
+            //The thrown exception can be used for even more detailed assertions.
+            Assert.Equal("Stack is Empty, Nothing to Pop", exception.Message);
+        }
+
+        [Fact]
+        public void AfterTwoPushes_SizeIsTwo()
+        {
+            //Arrange
+
+            //Act
+            stack.Push(0);
+            stack.Push(0);
+
+            //Assert
+            Assert.Equal(2, stack.GetSize());
+        }
+
+        [Fact]
+        public void AfterOnePushAndOnePop_IsEmpty()
+        {
+            //Arrange
+
+            //Act
+            stack.Push(0);
+            stack.Pop();
+
+            //Assert
+            Assert.True(stack.IsEmpty());
+            Assert.Equal(0, stack.GetSize());
+        }
+
+        [Fact]
+        public void AfterOnePush_IsNotEmpty()
+        {
+            //Arrange
+
+            //Act
+            stack.Push(0);
+
+            //Assert
+            Assert.False(stack.IsEmpty());
+            Assert.Equal(1, stack.GetSize());
+        }
+    }
+}
