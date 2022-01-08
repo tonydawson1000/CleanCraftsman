0b19565004cfd24ea70260f345a752ae6cf2b3f9
tonydawson1000 <tonydawson1000@gmail.com>
2022-01-08 15:55:16 +0000

    Added 10 Pin Bowling Game using TDD

---
+++ CleanCraftsman/Game.cs
@@ -0,0 +1,77 @@
+﻿namespace CleanCraftsman
+{
+    public class Game
+    {
+        //Bowling Game Rules
+
+        // If the Frame is a Strike, the Score is 10 plus the next two Balls
+        // If the Frame is a Spare, the Score is 10 plus the next Ball
+        // Otherwise the Score is the two Balls in the Frame
+
+        // 10 Frames in each Game
+        // 2 Balls in each Frame - EXCEPT 10th Frame - which has 3
+        // Total Rolls for a Game = 21
+
+        private int score = 0;
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            //Accumulate all the pins for each Roll
+            rolls[currentRoll++] = pins;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private int TwoBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        public int Score()
+        {
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if(IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+
+                    frameIndex += 2;
+                }
+                else if(IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    
+                    frameIndex++;
+                }
+                else
+                {
+                    score += TwoBallsInFrame(frameIndex);
+
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ CleanCraftsmanTests/BowlingTest.cs
@@ -0,0 +1,105 @@
+﻿using CleanCraftsman;
+using Xunit;
+
+namespace CleanCraftsmanTests
+{
+    public class BowlingTest
+    {
+        //Arrange
+        private readonly Game game;
+
+        public BowlingTest()
+        {
+            //Arrange
+            game = new Game();
+        }
+
+        private void RollMany(int frames, int pins)
+        {
+            for (int i = 0; i < frames; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            RollMany(2, 5);
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            //Act
+            // 10 Strikes + 2 x 10's
+            RollMany(12, 10);
+
+            //Assert
+            Assert.Equal(300, game.Score());
+        }
+
+        [Fact]
+        public void OneStrike()
+        {
+            //Act
+            RollStrike();
+            game.Roll(2);
+            game.Roll(3);
+
+            RollMany(16, 0);
+
+            //Assert
+
+            //      15 for Spare (10) + 2 + 3
+            // +     5  (2 + 3) 
+            // =    20
+            Assert.Equal(20, game.Score());
+        }
+
+        [Fact]
+        public void OneSpare()
+        {
+            //Act
+            RollSpare();
+            game.Roll(7);
+
+            RollMany(17, 0);
+
+            //Assert
+
+            //      17 for Spare (5+5) + 7
+            // +     7 
+            // =    24
+            Assert.Equal(24, game.Score());
+        }
+
+        [Fact]
+        public void AllOnes()
+        {
+            //Score of 20 if each Roll equals 1
+
+            //Act
+            RollMany(20, 1);
+
+            //Assert
+            Assert.Equal(20, game.Score());
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            //20 zeros in a GutterGame
+
+            //Act
+            RollMany(20, 0);
+
+            //Assert
+            Assert.Equal(0, game.Score());
+        }
+    }
+}
